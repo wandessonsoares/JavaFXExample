2b83a534c933f7847850f848c309af1cb428f828
Wandesson Soares <wandessonsilvatsi@gmail.com>
2015-06-07 17:12:23 -0300

    adicionado uma ação de inicialização e o campo de visualizar/editar nome

--- src/controllers/ChatController.java
+++ src/controllers/ChatController.java
@@ -7,7 +7,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
@@ -18,7 +20,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import models.TableModel;
 
-public class ChatController {
+public class ChatController implements Initializable{
 	
 	private StringBuffer msgs = new StringBuffer();
 	
@@ -48,18 +50,21 @@ public class ChatController {
     
     @FXML
     private TableColumn tcUsuarios;
+    
+    @FXML
+    private Label lbNome;
 
 
     @FXML
     void atualizar(MouseEvent event) {
-    	ObservableList<TableModel> usuarios = FXCollections.observableArrayList();
-    	usuarios.add(new TableModel("wandesson"));
-    	usuarios.add(new TableModel("alex"));
-    	usuarios.add(new TableModel("luciano"));
-    	
-    	tcUsuarios.setCellValueFactory(new PropertyValueFactory("nome"));
-    	
-    	tbOnline.setItems(usuarios);
+//    	ObservableList<TableModel> usuarios = FXCollections.observableArrayList();
+//    	usuarios.add(new TableModel("wandesson"));
+//    	usuarios.add(new TableModel("alex"));
+//    	usuarios.add(new TableModel("luciano"));
+//    	
+//    	tcUsuarios.setCellValueFactory(new PropertyValueFactory("nome"));
+//    	
+//    	tbOnline.setItems(usuarios);
     }
     
     @FXML
@@ -77,8 +82,21 @@ public class ChatController {
     	txChat.setText(msgs.toString());
     }
     
+    @Override
     public void initialize(URL url, ResourceBundle rb){
+    	ObservableList<TableModel> usuarios = FXCollections.observableArrayList();
+    	usuarios.add(new TableModel("wandesson"));
+    	usuarios.add(new TableModel("alex"));
+    	usuarios.add(new TableModel("luciano"));
     	
+    	tcUsuarios.setCellValueFactory(new PropertyValueFactory("nome"));
+    	
+    	tbOnline.setItems(usuarios);
+    }
+    
+    @FXML
+    void editarNome(MouseEvent event) {
+    	System.out.println("editar nome");
     }
 }
 
